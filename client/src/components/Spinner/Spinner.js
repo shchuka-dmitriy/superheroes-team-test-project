774536bc009f235c5e4c10217784549f170380a1
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-10-06 04:33:43 +0300

    Create Spinner component for waiting downloads
      - add Spinner.js
      - add Spinner.module.sass

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ClipLoader} from 'react-spinners';
+import {css} from '@emotion/core';
+import styles from './Spinner.module.sass';
+
+const override = css`border-color: #46568a;`;
+
+/**
+ * @description Shows a spinner while the page is loading
+ * @return {*}
+ */
+const SpinnerLoader = () => {
+    return (
+        <div className={styles.loaderContainer}>
+            <ClipLoader
+                sizeUnit={'px'}
+                css={override}
+                size={50}
+                color={'#46568a'}
+                loading={true}
+            />
+        </div>
+    )
+};
+
+export default SpinnerLoader;
