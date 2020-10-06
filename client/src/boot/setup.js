8bc69b8efa19a275c86ea03ad9fa147d138e3c86
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-10-06 04:26:47 +0300

    Create configuration for React Store
      - add configureStore.js
      - add setup.js

---
+++ client/src/boot/setup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import configureStore from './configureStore';
+import App from '../App';
+
+class Setup extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            store: configureStore()
+        };
+    }
+
+    render(){
+        return(
+            <Provider store={this.state.store}>
+                <App/>
+            </Provider>
+        )
+    }
+}
+
+export default Setup;
